ff2cb855c416c07132e9a03a5c5f91ef647568a9
Alberto45446 <76670453+Alberto45446@users.noreply.github.com>
2023-09-19 20:24:13 +0300

    dada

--- pages/index.js
+++ pages/index.js
@@ -1,75 +1,53 @@
 // pages/index.js
 
-import Link from 'next/link'
+import Link from 'next/link';
+
+import styles from './home.module.css';
 
 export default function Home() {
 
   return (
-    <div>
+    <div className={styles.hero}>
 
-      <div className="hero">
-        <h1>Vivere Audere</h1>
+      <h1 className={styles.title}>Vivere Audere</h1>
 
-        <p>An exciting new clothing brand launching soon!</p>
+      <p className={styles.text}>An exciting new clothing brand launching soon!</p>
+      
+      <div className={styles.emailSignup}>
+        <h2>Get notified when we launch</h2>
 
-        <div className="email-signup">
-          <h2>Get notified when we launch</h2>
-          
-          <form>
-            <input placeholder="Email address" />
-            <button>Submit</button>
-          </form>
-        </div>
-        
-        <Link href="/about">
-          <a>Learn more</a> 
-        </Link>
+        <form>
+          <input placeholder="Email address" />
+          <button>Submit</button> 
+        </form>
       </div>
 
-      <section>
-        <div className="brand">
+      <Link href="/about">
+        <a>Learn more</a>
+      </Link>
+
+      <section className={styles.section}>
+
+        <div className={styles.brand}>
+          
           <h2>Vivere Audere</h2>
+          
           <p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla mollis dapibus nunc, ut rhoncus turpis sodales quis. Integer sit amet mattis quam.</p>
+
         </div>
-        
-        <div className="vision">
+
+        <div className={styles.vision}>
+
           <h3>Our Vision</h3>
+
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  
           </p>
+
         </div>
+
       </section>
 
     </div>
   )
 }
-
-// Styles
-
-.hero {
-  background-image: url('/images/hero-bg.jpg');
-  background-size: cover;
-  padding: 100px;
-  text-align: center;
-  color: white; 
-}
-
-h1 {
-  font-size: 42px; 
-}
-
-p {
-  font-size: 18px;
-}
-
-// New styles
-
-section {
-  padding: 60px;
-}
-
-.brand {
-  font-size: 24px; 
-}
-
-// etc...
