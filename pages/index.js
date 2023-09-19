ff9e71a966dd03662e70aaa49a3f9428b5754a9c
Alberto45446 <76670453+Alberto45446@users.noreply.github.com>
2023-09-19 20:03:33 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,62 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+// pages/index.js
+
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
+    <div className="hero">
+      <h1>Vivere Audere</h1>
+      
+      <p>An exciting new clothing brand launching soon!</p>
+      
+      <div className="email-signup">
+        <h2>Get notified when we launch</h2>
+        
+        <form>
+          <input placeholder="Email address" />
+          <button>Submit</button>
+        </form>
+      </div>
+      
+      <Link href="/about">
+        <a>Learn more</a> 
+      </Link>
+      
+      <style jsx>{`
+        .hero {
+          background-image: url('/images/hero-bg.jpg');
+          background-size: cover;
+          padding: 100px;
+          text-align: center; 
+          color: white;
+        }
+        
+        h1 {
+          font-size: 42px;
+        }
+        
+        p {
+          font-size: 18px;
+        }
+        
+        .email-signup {
+          max-width: 400px;
+          margin: 20px auto;
+        }
+        
+        input {
+          padding: 10px;
+          width: 100%;
+          margin-bottom: 10px;
+        }
+        
+        button {
+          background: #f26a2e;
+          color: white;
+          padding: 10px 15px;
+          border: 0; 
+        }
+      `}</style>
     </div>
   )
 }
