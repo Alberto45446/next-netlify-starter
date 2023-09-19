1a43aa2ed971fd75a474aef4ef1137c774a6f0b6
Alberto45446 <76670453+Alberto45446@users.noreply.github.com>
2023-09-19 20:08:41 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,60 +3,73 @@
 import Link from 'next/link'
 
 export default function Home() {
+
   return (
-    <div className="hero">
-      <h1>Vivere Audere</h1>
-      
-      <p>An exciting new clothing brand launching soon!</p>
-      
-      <div className="email-signup">
-        <h2>Get notified when we launch</h2>
+    <div>
+
+      <div className="hero">
+        <h1>Vivere Audere</h1>
+
+        <p>An exciting new clothing brand launching soon!</p>
+
+        <div className="email-signup">
+          <h2>Get notified when we launch</h2>
+          
+          <form>
+            <input placeholder="Email address" />
+            <button>Submit</button>
+          </form>
+        </div>
         
-        <form>
-          <input placeholder="Email address" />
-          <button>Submit</button>
-        </form>
+        <Link href="/about">
+          <a>Learn more</a> 
+        </Link>
       </div>
-      
-      <Link href="/about">
-        <a>Learn more</a> 
-      </Link>
-      
-      <style jsx>{`
-        .hero {
-          background-image: url('/images/hero-bg.jpg');
-          background-size: cover;
-          padding: 100px;
-          text-align: center; 
-          color: white;
-        }
+
+      <section>
+        <div className="brand">
+          <h2>Vivere Audere</h2>
+          <p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla mollis dapibus nunc, ut rhoncus turpis sodales quis. Integer sit amet mattis quam.</p>
+        </div>
         
-        h1 {
-          font-size: 42px;
-        }
-        
-        p {
-          font-size: 18px;
-        }
-        
-        .email-signup {
-          max-width: 400px;
-          margin: 20px auto;
-        }
-        
-        input {
-          padding: 10px;
-          width: 100%;
-          margin-bottom: 10px;
-        }
-        
-        button {
-          background: #f26a2e;
-          color: white;
-          padding: 10px 15px;
-          border: 0; 
-        }
-      `}</style>
+        <div className="vision">
+          <h3>Our Vision</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+        </div>
+      </section>
+
     </div>
   )
 }
+
+// Styles
+
+.hero {
+  background-image: url('/images/hero-bg.jpg');
+  background-size: cover;
+  padding: 100px;
+  text-align: center;
+  color: white; 
+}
+
+h1 {
+  font-size: 42px; 
+}
+
+p {
+  font-size: 18px;
+}
+
+// New styles
+
+section {
+  padding: 60px;
+}
+
+.brand {
+  font-size: 24px; 
+}
+
+// etc...
